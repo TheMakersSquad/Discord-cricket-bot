90a45a0a7ba32ea585fc03615ebb4727175f5f7f
Bikram Parajuli <bikramparajuli0000@gmail.com>
2020-08-18 14:12:08 +0545

    Added today\'s matches

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ client.once("ready", () => {
 });
 
 client.on("message", (message) => {
-  if (message.content.startsWith(`${prefix}score`)) {
+  if (message.content.startsWith(`${prefix}detail`)) {
     console.log(message.content);
     var messageArray = message.content.split(" ");
     var matchID = parseInt(messageArray[1]);
-    var param = messageArray[2];
+    var param = messageArray[2] || "info";
     var path = `/matches/engine/match/${matchID}.json`;
     let url = `http://www.espncricinfo.com` + path;
     console.log(path);
@@ -27,10 +27,30 @@ client.on("message", (message) => {
           description: json["description"],
           series: json["series"][0]["series_name"],
           status: json["live"]["status"],
+          info: `${json["series"][0]["series_name"]} \n\n ${json["description"]} \n\n ${json["live"]["status"]}`,
         };
         message.channel.send(mappedParam[param]); // do something with JSON
       });
     console.log(matchID);
+  }
+  if (message.content.startsWith(`${prefix}matches`)) {
+    let url = "http://cricscore-api.appspot.com/csa";
+    let settings = { method: "Get" };
+    let finalMessage = "";
+    fetch(url, settings)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+
+        for (i = 1; i < json.length; i++) {
+          finalMessage += ` \`${json[i]["id"]}\` : ${json[i]["t1"]} vs ${json[i]["t2"]} \n`;
+        }
+        finalMessage =
+          " All the matches for today are: \n\n" +
+          finalMessage +
+          `\n Send \`${prefix}detail match_id param\` for details`;
+        message.channel.send(finalMessage); // do something with JSON
+      });
   }
 });
 
