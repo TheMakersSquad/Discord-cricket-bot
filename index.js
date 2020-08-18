ee1c37b697c81ccc20a878e2dfdb3532df6722b6
Bikram Parajuli <bikramparajuli0000@gmail.com>
2020-08-18 13:34:00 +0545

    Added basic functionality

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
+const fetch = require("node-fetch");
 
-const {prefix, token} = require('./config.json')
+const { prefix, token } = require("./config.json");
 
 const client = new Discord.Client();
 
-
-
-
-
-
-client.once('ready',()=>{
-    client.user.setActivity("Cricket", {type:"PLAYING"})
-    console.log('Bot is Ready')
+client.once("ready", () => {
+  client.user.setActivity("Cricket", { type: "PLAYING" });
+  console.log("Bot is Ready");
 });
 
-client.on('message',message=>{
-    if(message.content.startsWith(`${prefix}score`)){
-            message.channel.send(":wave:"+"Hello. Not there yet. Keep Patience")
-}});
-
+client.on("message", (message) => {
+  if (message.content.startsWith(`${prefix}score`)) {
+    console.log(message.content);
+    var messageArray = message.content.split(" ");
+    var matchID = parseInt(messageArray[1]);
+    var param = messageArray[2];
+    var path = `/matches/engine/match/${matchID}.json`;
+    let url = `http://www.espncricinfo.com` + path;
+    console.log(path);
+    let settings = { method: "Get" };
+    fetch(url, settings)
+      .then((res) => res.json())
+      .then((json) => {
+        let mappedParam = {
+          description: json["description"],
+          series: json["series"][0]["series_name"],
+          status: json["live"]["status"],
+        };
+        message.channel.send(mappedParam[param]); // do something with JSON
+      });
+    console.log(matchID);
+  }
+});
 
 client.login(token);
